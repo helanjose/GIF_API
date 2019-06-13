36f288122e691eeb371bdaac6a5b7f81f415b80f
strebz <helankjose@gmail.com>
2019-06-13 08:20:01 -0700

    changed allignments

--- giphy.js
+++ giphy.js
@@ -4,94 +4,92 @@ $(document).ready(function(){
 var animals = ["parrot", "elephant", "cat", "dog"];
 console.log(animals);
 
-// Function for displaying movie data
+
 function addButtons() {
 
-  // Deleting the movie buttons prior to adding new movie buttons
-  // (this is necessary otherwise we will have repeat buttons)
-  $("#buttons-view").empty();
+  
+   $("#buttons-view").empty();
 
-  // Looping through the array of movies
-  for (var i = 0; i < animals.length; i++) {
+    // Looping through the array of animals
+    for (var i = 0; i < animals.length; i++) {
 
-    // Then dynamicaly generating buttons for each movie in the array.
-    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adding a class
-    a.addClass("animalz");
-    // Adding a data-attribute with a value of the movie at index i
-    a.attr("data-name", animals[i]);
-    // Providing the button's text with a value of the movie at index i
-    a.text(animals[i]);
-    console.log($(".animalz").text());
-
+    // Then dynamicaly generating buttons for each value in the array.
     
-    // Adding the button to the HTML
-    $("#buttons-view").append(a);
+      var a = $("<button>");
+      // Adding a class
+      a.addClass("animalz");
+      // Adding a data-attribute with a value of  animal at index i
+      a.attr("data-name", animals[i]);
+      // Providing the button's text with a value of animal at index i
+      a.text(animals[i]);
+      console.log($(".animalz").text());
+
+   
+      // Adding the button to the HTML
+      $("#buttons-view").append(a);
   }
 }
 
 // This function handles events where one button is clicked
 $("#add-animal").on("click", function(event) {
   // event.preventDefault() prevents the form from trying to submit itself.
-  // We're using a form so that the user can hit enter instead of clicking the button if they want
-  event.preventDefault();
+  
+    event.preventDefault();
 
-  // This line will grab the text from the input box
-  var animal = $("#animal-input").val().trim();
-  $("#animal-input").html('<h1>ada</h1>');
-  // The movie from the textbox is then added to our array
-  animals.push(animal);
+    // This line will grab the text from the input box
+    var animal = $("#animal-input").val().trim();
+    $("#animal-input").html('<h1>ada</h1>');
+    // The animal name from the textbox is then added to our array
+    animals.push(animal);
 
-  // calling renderButtons which handles the processing of our movie array
-  addButtons();
+    // calling addButtons which handles the processing of animal array
+    addButtons();
 });
 
-// Calling the renderButtons function at least once to display the initial list of movies
 
 
- function displayInfo() {
+
+function displayInfo() {
    // console.log($(".animalz").text());
-  var animal_name = $(this).text();
-  console.log(animal_name);
+    var animal_name = $(this).text();
+    console.log(animal_name);
 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    animal_name + "&api_key=a6cl8TDa40alTpt5acXal9kQ4lRWQeHH&limit=10";
 
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  animal_name + "&api_key=a6cl8TDa40alTpt5acXal9kQ4lRWQeHH&limit=10";
-
-// Performing an AJAX request with the queryURL
-$.ajax({
+    // Performing an AJAX request with the queryURL
+  $.ajax({
   url: queryURL,
   method: "GET"
-})
+  })
   // After data comes back from the request
   .then(function(response) {
-    console.log(queryURL);
+      console.log(queryURL);
 
-    console.log(response);
-    // storing the data from the AJAX request in the results variable
-    var results = response.data;
+      console.log("response:"+response);
+     // storing the data from the AJAX request in the results variable
+      var results = response.data;
 
-    // Looping through each result item
-    for (var i = 0; i < results.length; i++) {
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
 
-      // Creating and storing a div tag
-      var animalDiv = $("<div>");
+            // Creating and storing a div tag
+          var animalDiv = $("<div>");
 
-      // Creating a paragraph tag with the result item's rating
-      var p = $("<p>").text("Rating: " + results[i].rating);
+            // Creating a paragraph tag with the result item's rating
+          var p = $("<p>").text("Rating: " + results[i].rating);
 
-      // Creating and storing an image tag
-      var animalImage = $("<img>");
-      // Setting the src attribute of the image to a property pulled off the result item
-      animalImage.attr("src", results[i].images.fixed_height.url);
+            // Creating and storing an image tag
+          var animalImage = $("<img>");
+          // Setting the src attribute of the image to a property pulled off the result item
+          animalImage.attr("src", results[i].images.fixed_height.url);
 
-      // Appending the paragraph and image tag to the animalDiv
-      animalDiv.append(p);
-      animalDiv.append(animalImage);
+          // Appending the paragraph and image tag to the animalDiv
+          animalDiv.append(p);
+          animalDiv.append(animalImage);
 
-      // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-      $("#gifs-appear-here").prepend(animalDiv);
+          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+          $("#gifs-appear-here").prepend(animalDiv);
     }
   });
   
@@ -100,7 +98,7 @@ $(document).on("click", ".animalz", displayInfo);
 
 
 
-// Calling the renderButtons function to display the intial buttons
+// Calling the addButtons function to display the intial buttons
 addButtons();
 
 });
